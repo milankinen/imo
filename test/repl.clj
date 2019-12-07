cb0a49e8d74ba990e2eff946d2fe587859b3ad5a
Matti Lankinen <m.lankinen@iki.fi>
2019-12-07 22:17:38 +0200

    Add some convenience functions for REPL usage

--- test/repl.clj
+++ test/repl.clj
@@ -1,4 +1,15 @@
-(ns repl)
+(ns repl
+  (:require [imo.reader :as reader]
+            [clojure.string :as string]))
+
+(defn ast [s]
+  (->> (string/split-lines s)
+       (map #(string/replace % #"^\s*\|" ""))
+       (string/join "\n")
+       (reader/read-ast)))
+
+(defn pr-ast [ast]
+  (reader/print-ast ast))
 
 (comment
   ; Do experiments here but do not commit ;-)
