2d85ce127d4795ab2ed8c7823ab0460d68cd039a
Matti Lankinen <m.lankinen@iki.fi>
2020-05-16 14:22:03 +0300

    Rename smoke test namespace

--- test/imo/test/smoke_tests.clj
+++ test/imo/test/smoke_tests.clj
@@ -1,14 +1,16 @@
-(ns imo.test.preprocessing-tests
+(ns imo.test.smoke-tests
   (:require [clojure.test :refer :all]
             [imo.analysis :as analysis]
             [imo.test-utils :refer [load-test-file]]
             [imo.reader :as reader]
             [imo.util :refer [node->source]]))
 
-(deftest clj-core-parsing-and-analysis-test
-  (testing "parsing and analysis preserve ast so that ast can be rendered back to the source"
+(deftest reader-and-analysis-data-preserve-test
+  (testing "reading and analysis do not drop any data from the read ast"
     (let [input (load-test-file "clojure_core.clj")
           output (-> (reader/read-ast input)
                      (analysis/analyze-ast)
                      (node->source))]
+      ; If we can construct the input source back from analyzed ast,
+      ; we know that we haven't dropped any relevant info
       (is (= input output)))))
